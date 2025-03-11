d5380c8487f8a8e3a92b751e9e3b7862782353c4
cburitho <40325@prot.ac.th>
2025-03-11 14:58:30 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("first number : "))
+num2 = int(input("second number : "))
+
+print("Thx.")
+print(num1 ,"+",num2, "=", num1+num2)
+print(num1 ,"-",num2, "=", num1-num2)
+print(num1 ,"*",num2, "=", num1*num2)
+print(num1 ,"/",num2, "=", num1/num2)
