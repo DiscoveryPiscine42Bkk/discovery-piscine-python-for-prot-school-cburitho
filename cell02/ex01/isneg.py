c1e2149197a3d8cdaff2b38a865e7bedc239ad55
cburitho <40325@prot.ac.th>
2025-03-11 09:58:59 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,7 @@
+number = int(input("please enter your number"))
+if number == 0 :
+    print("this number is born side negative and positive")
+if number > 0 :
+    print ("this number positive")
+if number < 0 :
+    print("this number negative")
