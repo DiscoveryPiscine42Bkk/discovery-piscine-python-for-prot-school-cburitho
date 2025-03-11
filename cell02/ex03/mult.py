6a8ac3266c98557a47de0211cd3223a065ff22c0
cburitho <40325@prot.ac.th>
2025-03-11 10:47:23 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+number2 = int(input("Please enter your first number"))
+number8 = int(input("Please input your second number"))
+
+result = number2 * number8
+ print(number2,"x",number8,"=",result)
+
+if result == 0 :
+ print("the result is both side negative and positive")
+
+if result > 0 :
+ print("the result is positive")
+if result < 0 :
+ print("the result is negative")
