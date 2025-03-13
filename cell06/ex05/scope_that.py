ddff67efc99c2ab47f1a39b2b92d843c08bca5e0
cburitho <40325@prot.ac.th>
2025-03-13 09:58:53 +0700

    Add files via upload

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+def add_one(number):
+    return number + 1
+def main():
+    my_var + 5
+    print(f"Initial value of my_var: {my_var}")
+    my_var = add_one(my_var)
+    print(f"Updated value of my_var after add_one: {my_var}")
+if __name__ == " __main__ ":
+    main()
